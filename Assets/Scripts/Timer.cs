220d3c216cb425f71b71bef790709a50d60f0cf8
SolRomero1999 <128836209+SolRomero1999@users.noreply.github.com>
2024-08-01 18:47:32 -0300

    inicio iu

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText; 
+    public float timeRemaining = 60; 
+
+    private bool timerIsRunning = false;
+
+    private void Start()
+    {
+        timerIsRunning = true; 
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
+
